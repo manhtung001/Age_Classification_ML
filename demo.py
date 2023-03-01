3f4b75f40e6661ef6bafead534cc055b7b5ce72b
manhtung001 <khongtung001@gmail.com>
2023-03-01 17:07:49 +0700

    add show detail

--- demo.py
+++ demo.py
@@ -14,14 +14,14 @@ with open("utils/knn_model.pkl", 'rb') as file:
 
 detector = cv2.CascadeClassifier("utils/haarcascade_frontalface_default.xml")
 
-LIST_FACES = []
-
 
 N = 6480
 orientations = 5
 pixels_per_cell = (16, 16)
 cells_per_block = (4, 4)
 
+
+LIST_FACES = []
 
 def inference_choose_face(img):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -77,32 +77,14 @@ def inference_predict_age(img, face_nums):
         cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.putText(image, "#{}-pred: {}".format(index, predict), (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
     
-    return image
+    return image, gr.Dropdown.update(choices=[str(i) for i in face_nums])
 
 
-# def inference_create_output(img):
-#     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
-#     print("inference_create_output")
-
-#     img = img.astype('float32') / 255
-#     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#     img = np.transpose(img, (2, 0, 1))
-
-#     masks = MASK * 255
-
-#     batch = dict(image=img, mask=masks[None, ...])
-
-#     batch['unpad_to_size'] = [torch.tensor([batch['image'].shape[1]]),torch.tensor([batch['image'].shape[2]])]
-#     batch['image'] = torch.tensor(pad_img_to_modulo(batch['image'], predict_config.dataset.pad_out_to_modulo))[None].to(predict_config.device)
-#     batch['mask'] = torch.tensor(pad_img_to_modulo(batch['mask'], predict_config.dataset.pad_out_to_modulo))[None].float().to(predict_config.device)
-
-#     cur_res = refine_predict(batch, inpaint_model, **predict_config.refiner)
-#     cur_res = cur_res[0].permute(1,2,0).detach().cpu().numpy()
-
-#     cur_res = np.clip(cur_res * 255, 0, 255).astype('uint8')
-
-#     return cur_res
+def inference_see_detail(img, see_detail):
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    print("inference_see_detail")
+    print(see_detail)
+    return img
 
 
 demo = gr.Blocks()
@@ -112,24 +94,24 @@ options = []
 
 with demo:
     img_input = gr.Image(label='input')
-    # class_name = gr.Dropdown(list(className.keys()),value='person',label= 'class name')
-    # confidence_score = gr.Slider(0, 1, value=0.5, step=0.05,label='confidence score')
+    
     b1 = gr.Button("detect all faces")
 
     img_input_1 = gr.Image(label='face prediction')
     face_nums = gr.Dropdown([], value=[], multiselect=True)
-    # gaussian_blur = gr.Slider(1, 20, value=7, step=1,label='gaussian blur kernel size')
-    # mask_threshold = gr.Slider(0, 1, value=0.2, step=0.05,label='mask threshold')
+    
     b2 = gr.Button("predict age")
     
     img_predict = gr.Image(label='img face prediction')
 
-    b3 = gr.Button("run")
+    see_detail_option = gr.Dropdown(options, label="options to see detail")
 
-    img_output = gr.Image(label='img_output')
+    b3 = gr.Button("see detail")
+
+    img_detail = gr.Image(label='img_detail')
 
     b1.click(inference_choose_face, inputs=[img_input], outputs=[img_input_1, face_nums])
-    b2.click(inference_predict_age, inputs=[img_input, face_nums], outputs=img_predict)
-    # b3.click(inference_create_output, inputs=[img_input], outputs=img_output)
+    b2.click(inference_predict_age, inputs=[img_input, face_nums], outputs=[img_predict, see_detail_option])
+    b3.click(inference_see_detail, inputs=[img_input, see_detail_option], outputs=img_detail)
 
 demo.launch(debug=True)
